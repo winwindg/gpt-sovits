5dc8a4ae8c14eaca91efe2232da199a4d58a3bae
Carver Gu <carver.gu@qq.com>
2025-04-10 20:57:36 +0800

    match loudness

--- tools/cmd-denoise.py
+++ tools/cmd-denoise.py
@@ -1,6 +1,8 @@
 import os
 import argparse
 import traceback
+import soundfile as sf
+import pyloudnorm as pyln
 
 from modelscope.pipelines import pipeline
 from modelscope.utils.constant import Tasks
@@ -18,8 +20,30 @@ def execute_denoise(input_folder, output_folder):
     for name in tqdm(os.listdir(input_folder)):
         try:
             ans("%s/%s" % (input_folder, name), output_path="%s/%s" % (output_folder, name))
+            match_loudness(f"{input_folder}/{name}", f"{output_folder}/{name}")
         except:
             traceback.print_exc()
+
+
+def match_loudness(ref_file, target_file):
+    # Load the reference audio (original, unprocessed)
+    ref_audio, rate = sf.read(ref_file)
+
+    # Load the target audio (denoised version)
+    target_audio, _ = sf.read(target_file)
+
+    # Create a loudness meter using the sample rate
+    meter = pyln.Meter(rate)
+
+    # Measure integrated loudness (LUFS) of both files
+    ref_loudness = meter.integrated_loudness(ref_audio)
+    target_loudness = meter.integrated_loudness(target_audio)
+
+    # Normalize target audio to match reference loudness
+    target_audio_normalized = pyln.normalize.loudness(target_audio, target_loudness, ref_loudness)
+
+    # Save the normalized audio back to the target file (overwrite)
+    sf.write(target_file, target_audio_normalized, rate)
 
 
 if __name__ == "__main__":
